d2129894a542a76ae0b111514a9dac1ef8184015
jgeneaguilar <jgeneaguilar@gmail.com>
2019-07-14 12:55:55 +0800

    Setup up the accounts view in the Side Menu

--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import {
   Drawer,
@@ -9,10 +9,14 @@ import {
   ListItemText,
   Toolbar,
   Button,
+  Collapse
 } from '@material-ui/core';
 import AccountBalance from '@material-ui/icons/AccountBalance';
 import Assignment from '@material-ui/icons/Assignment';
 import AddCircleOutline from '@material-ui/icons/AddCircleOutline';
+import ExpandLess from '@material-ui/icons/ExpandLess';
+import ExpandMore from '@material-ui/icons/ExpandMore';
+import Edit from '@material-ui/icons/Edit';
 import { makeStyles } from '@material-ui/core/styles';
 
 
@@ -22,7 +26,7 @@ const useStyles = makeStyles({
     minHeight: '48px'
   },
   paper: {
-    width: '200px'
+    width: '240px'
   },
   drawer: {
     flexShrink: 0,
@@ -33,6 +37,13 @@ const useStyles = makeStyles({
   },
   addIcon: {
     marginRight: '5px'
+  },
+  primaryText: {
+    float: 'left'
+  },
+  secondaryText: {
+    float: 'right',
+    textAlign: 'right'
   }
 });
 
@@ -40,6 +51,18 @@ const SideMenu = ({ sideMenuOpen }) => {
 
   const classes = useStyles();
 
+  const [listOpen, setListOpen] = useState({
+    budgetList: null,
+    trackingList: null
+  });
+
+  // const handleClick = list => {
+  //   setListOpen({
+  //     ...listOpen,
+  //     [list]: true,
+  //   })
+  // }
+  
   return (
     <Drawer
       variant="persistent"
@@ -62,7 +85,59 @@ const SideMenu = ({ sideMenuOpen }) => {
           <ListItemText primary='Accounts' />
         </ListItem>
       </List>
+
       <Divider />
+      {/* Budget Accounts List */}
+      <List>
+        <ListItem button dense>
+          {listOpen ? <ExpandLess /> : <ExpandMore />}
+          <ListItemText 
+            primary='BUDGET' 
+            secondary='P 89,987'
+            classes={{ primary: classes.primaryText, secondary: classes.secondaryText }}  
+          />
+        </ListItem>
+        <Collapse in={listOpen} timeout='auto' unmountOnExit>
+          <List component='div' disablePadding>
+            <ListItem button dense>
+              <ListItemIcon>
+                <Edit fontSize='small'/>   
+              </ListItemIcon>
+              <ListItemText 
+                primary='BPI' 
+                secondary='P 55,000' 
+                classes={{ primary: classes.primaryText, secondary: classes.secondaryText }}
+                />
+            </ListItem>
+          </List>
+        </Collapse>
+      </List>
+
+      <Divider />
+      {/* Tracking Accounts List */}
+      <List>
+        <ListItem button dense>
+          {listOpen ? <ExpandLess /> : <ExpandMore />}
+          <ListItemText primary='TRACKING' />
+        </ListItem>
+        <Collapse in={listOpen} timeout='auto' unmountOnExit>
+          <List component='div' disablePadding>
+            <ListItem button dense>
+              <ListItemIcon>
+                <Edit /> 
+              </ListItemIcon>
+              <ListItemText 
+                primary='Mutual Fund' 
+                secondary='P 100,000'
+                classes={{ primary: classes.primaryText, secondary: classes.secondaryText }}
+              />
+            </ListItem>
+          </List>
+        </Collapse>
+      </List>
+
+      <Divider />
+
       <Button
         variant='contained'
         color='default'
@@ -81,3 +156,7 @@ SideMenu.propTypes = {
 };
 
 export default SideMenu;
+
+/** TO DO
+ * MuiTypography-body2 fontSize = '0.75rem'
+ */
