c05b0f5eac70b7d0c2f292b2a21601b086312032
Maica Albarando <maica@acombina.com>
2019-08-17 14:41:18 +0800

    Implement authenticated route

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, Fragment } from 'react';
 import './App.css';
 // import HomePage from './components/home/HomePage';
 import LoginPage from './components/login/LoginPage';
@@ -6,10 +6,17 @@ import DashboardContainer from './components/dashboard/DashboardContainer';
 import NotFound from './components/NotFound';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
+import AuthenticatedRoute from './commons/AuthenticatedRoute';
+import { checkUserLogin } from './redux/actions/userActions';
+import { Empty } from 'antd';
 
 
-function App({ isLoggedIn }) {
-  
+function App({ isLoggedIn, checkUserLogin }) {
+
+  useEffect(() => {
+    checkUserLogin();
+  }, []);
+
   const redirect = isLoggedIn ? (
     <Redirect to='/dashboard' />
   ) : (
@@ -17,12 +24,18 @@ function App({ isLoggedIn }) {
     )
 
   return (
-    <Switch>
-      <Route path='/' exact render={() => redirect }/>
-      <Route exact path='/dashboard' component={DashboardContainer}/>
-      <Route path='/login' render={() => redirect }/>
-      <Route component={NotFound}/>
-    </Switch>
+    <Fragment>
+      { 
+        // TODO: Implement Loading Screen
+        isLoggedIn === null ? <Empty /> : 
+        (<Switch>
+        <AuthenticatedRoute path='/' exact />
+        <AuthenticatedRoute exact path='/dashboard' component={DashboardContainer} />
+        <Route path='/login' render={() => redirect}  />
+        <Route component={NotFound} />
+      </Switch>)
+      }
+    </Fragment>
   );
 }
 
@@ -32,4 +45,4 @@ function mapStateToProps(state) {
   }
 }
 
-export default connect(mapStateToProps)(App);
+export default connect(mapStateToProps, { checkUserLogin })(App);
---
+++ src/commons/AuthenticatedRoute.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Redirect, Route } from 'react-router-dom';
+
+const AuthenticatedRoute = ({ isLoggedIn, component: Component, ...props }) => {
+
+  const redirect = isLoggedIn ? (
+    <Component {...props} />
+  ) : (
+      <Redirect to={{
+        pathname: '/login/',
+        state: { from: props.location }
+      }} />
+    )
+
+  return (
+    <Route {...props} render={() => redirect} />
+  )
+}
+
+function mapStateToProps(state) {
+  return {
+    isLoggedIn: state.user.isLoggedIn
+  }
+};
+
+export default connect(mapStateToProps)(AuthenticatedRoute);
